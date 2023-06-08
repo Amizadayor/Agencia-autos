ab610de740f0ba75e13e3cf03b60d0cad42de98d
Amizaday OR <juanamizadayo@gmail.com>
2023-06-08 00:18:04 -0600

    Creación de los Controladores

---
+++ app/Http/Controllers/AutoCategoriaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AutoCategoria;
+use Illuminate\Http\Request;
+
+class AutoCategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AutoCategoria $autoCategoria)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AutoCategoria $autoCategoria)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AutoCategoria $autoCategoria)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AutoCategoria $autoCategoria)
+    {
+        //
+    }
+}
